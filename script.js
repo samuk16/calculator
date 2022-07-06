5a93ccbff36c69b6c2e1bffdc8e8211b03eeb998
Samuk <marcelolamas19@gmail.com>
2022-07-05 22:30:22 -0300

    Add if statement to make calculations

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 
 function add(a,b){
-    return a + b
+    return console.log(a + b);
     // let sum = a + b;
     // return sum;
     // console.log(sum);
 }
 
-function subtract(a,b){return a - b}
+function subtract(a,b){return console.log(a - b)}
 
-function multiply(a,b){return a * b}
+function multiply(a,b){return console.log(a * b)}
 
 function divide(a,b){return a / b}
 
@@ -41,29 +41,51 @@ function operate(o,n1,n2){
     }
 }
 
-let tempValue;
+let tempValue1 ;
 
 let containerNum = document.querySelectorAll('.containerNum');
 
-let numAndOperators = [0,1,2,3,4,5,6,7,8,9,0];
-let operators = ['+','-','*','/'];
-let prueba3  = [];
-let prueba4 = [] ;
+let numAndOperators = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '0':0,
+};
+let operators = {
+    '+':'+',
+    '-':'-',
+    '*':'*',
+    '/':'/',
+};
+let pruebaNum3  = [];
+let pruebaOp6  = [];
+let pruebaNum5  = [];
+
+let isEmpty = true;
+
+let prueba4 = {
+    name:'nashe',
+};
+
+
 containerNum.forEach(item => item.addEventListener('click', e => {
 
-
-    // console.log(e.target.value)
-    // if(e.target.classList.contains('n1')){
-    //     console.log(1);
-    // }
-
+    /*
+    let nashe23 = [e.target.value];
     
     function prueba(arr,num){
 
        let prueba2 = arr.map(a => {
             if(a == num){
-                console.log(num);
-                prueba4 = prueba3.push(num);
+                // console.log(num);
+                // prueba4 = prueba3.push(num);
+                tempValue1 = nashe23;
                 
             }
         });
@@ -71,9 +93,56 @@ containerNum.forEach(item => item.addEventListener('click', e => {
         
     }
     prueba(numAndOperators,e.target.value);
-    console.log(prueba3);
-    console.log(prueba4);
+    // console.log(prueba3);
+    console.log(tempValue1);
+    */
+    let tempE = e.target.value;
+
+    if(tempE in numAndOperators){
+
+        if(isEmpty == true){
+            let count = pruebaNum3.push(e.target.value);
+            console.log(pruebaNum3);
+
+        }else{
+            let count = pruebaNum5.push(e.target.value);
+            console.log(pruebaNum5);
+        }
+        // console.log(e.target.value);
+       
+        // prueba4.tempNum = [e.target.value];
+        // console.log(prueba4);
+
+    }else if(e.target.value in operators){
+
+        let count2 = pruebaOp6.push(e.target.value);
+        console.log(pruebaOp6);
+        isEmpty = false;
+        // prueba4.tempOper = e.target.value;
+        // console.log(prueba4)
+
+    }else if(tempE === '='){
 
 
+        // console.log('=')
+
+        const returnInt = (element) => parseInt(element,10);
+
+        let nashe3 = pruebaNum3.map(returnInt).join('');
+        let nashe4 = pruebaNum5.map(returnInt).join('');
+        let opFinal = pruebaOp6.join('');
+
+        console.log(nashe3)
+        console.log(nashe4)
+        console.log(operate(opFinal,nashe3,nashe4));
+
+        // console.log(nashe3.join(''));
+
+    
+
+
+    }
+    
+   
 }))
 
